6ad9e81bb32801921bd4d5620cc9e052d78a854a
HeavenlySea <pavelking11@mail.ru>
2022-05-16 08:23:36 +0400

    [UPD]: init new logic

--- FUploader/main/service.py
+++ FUploader/main/service.py
@@ -1,28 +1,44 @@
 from os import path
+from string import ascii_letters
 
-class ProcessingText:
-    def __init__(self, file):
-        self.verifity = self.validation_to_file(file)
-        self.__file = file
-         
-    def __str__(self):
-        return f"{self.__file}"
 
-    def _get_text(self) -> str:
-        return self.decode()
-
-    def _read_text(self) -> str:
-        return self.__file.read()
+def processong_text(file, options):
     
-    def decode(self) -> str:
-        return self._read_text().decode("UTF-8")
-
-    def validation_to_file(self, file) -> None:
-        # checking the file for extension and size
+    
+    def validation_to_file(file):
         if not path.splitext(file.name)[1] in (".txt"):
             raise "Invalid file extension"
         if len(file) == 0 or len(file) < 0:
             raise "The file is empty"
         elif len(file) >= 1000000:
             raise "The file size is too large"
-        return "Verification passed"
+        return ("Verification passed", True)
+
+    if True in validation_to_file(file=file):
+        _file = file.read().decode('utf-8')
+        if options == "d_space":
+            _file = _file.replace('\n', ' ').replace(' ', '')
+            return _file.rstrip().strip().rstrip()
+        elif options == "d_symbols":
+            pass
+    
+    
+    
+    
+
+# class ProcessingText:
+#     def __init__(self, file):
+#         self.verifity = self.validation_to_file(file)
+#         self.__file = file
+         
+#     def __str__(self):
+#         return f"{self.__file}"
+
+#     def _get_text(self) -> str:
+#         return self.decode()
+
+#     def _read_text(self) -> str:
+#         return self.__file.read()
+    
+#     def decode(self) -> str:
+#         return self._read_text().decode("UTF-8")
--- FUploader/main/views.py
+++ FUploader/main/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
-from .forms import FileUploadForm
-from .service import ProcessingText
+from .forms import FileUploadForm, FileUploadOptionsForm
+from .service import processong_text
 
 
 def index(request):
     if request.method == 'POST':
+        options_form = FileUploadOptionsForm(request.POST)
         form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            content = ProcessingText(request.FILES["input_file"])._get_text()
+        if form.is_valid() or options_form.is_valid():
+            get_option = request.POST['radio_button']
+            get_file = request.FILES["input_file"]
+            content = processong_text(get_file, get_option)
             return render(request, 'main/main.html', {"text": content})
     else:
+        options_form = FileUploadOptionsForm()
         form = FileUploadForm()
-    return render(request, 'main/index.html', {'form': form})
+    return render(request, 'main/index.html', {'form': form, 'options_form': options_form})
